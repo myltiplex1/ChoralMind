b1e5cc4affbe12128a3d3800cfdfc5375c920827
Soyinka Enoch <timflash111@gmail.com>
2025-08-09 01:42:12 +0100

    render push

--- gradio_app.py
+++ gradio_app.py
@@ -1,4 +1,5 @@
 import time
+import os
 import gradio as gr
 from retriever.retriever import HymnRetriever
 from llm.generate_response import generate_hymn_response
@@ -103,4 +104,5 @@ with gr.Blocks() as demo:
     )
 
 if __name__ == "__main__":
-    demo.launch(pwa=True, favicon_path="assets/icon-192x192.png")
+    port = int(os.environ.get("PORT", 7860))
+    demo.launch(server_name="0.0.0.0", server_port=port, pwa=True, favicon_path="assets/icon-192x192.png")
